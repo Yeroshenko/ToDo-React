ab0ba560234592cead4600608cc316a51461f257
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-01-14 02:36:26 +0200

    Done

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,7 +18,9 @@ export default class App extends Component {
             this.createTodoItem('Make awesome App'),
             this.createTodoItem('Have a lunch'),
             this.createTodoItem('Sleep')
-        ]
+        ],
+        term: '',
+        filter: 'all' // all || active || done
     }
 
     createTodoItem(label) {
@@ -61,7 +63,6 @@ export default class App extends Component {
     }
     
     toggleProperty(arr, id, propName) {
-        this.setState(({todoData}) => {
             const idx = arr.findIndex((el) => el.id === id)
 
             const oldItem = arr[idx]
@@ -72,7 +73,7 @@ export default class App extends Component {
                 newItem,
                 ...arr.slice(idx + 1)
             ]
-        })
+        
     }
 
     onToggleDone = (id) => {
@@ -82,49 +83,78 @@ export default class App extends Component {
             }
         })
     }
-
     
     onToggleImportant = (id) => {
         this.setState(({ todoData }) => {
             return { 
-                todoData: this.toggleProperty(todoData, id, 'done')
+                todoData: this.toggleProperty(todoData, id, 'important')
             }
         })
     }
 
+    search(items ,term) {
+
+        if (term.label === 0) {
+            return items
+        }
+
+        return items.filter((item) => {
+            return item.label
+                .toLowerCase()
+                .indexOf(term.toLowerCase()) > -1
+        })
+    }
+
+    onSearchChange = (term) => {
+        this.setState({ term })
+    }
+
+    onFilterChange = (filter) => {
+        this.setState({ filter })
+    }
+
+    filter(items, filter) {
+
+        switch(filter){
+            case 'all':
+                return items
+            case 'active':
+                return items.filter((items) => !items.done)
+            case 'done':
+                return items.filter((items) => items.done)
+            default:
+                return items
+        }
+    }
+
     render() {
-        const { todoData } = this.state
-//  ------------------- 
+        const { todoData, term, filter } = this.state
 
-        if (!todoData) {
-            throw Error('отсутствует todoData')
-          }
-          if (!Array.isArray(todoData)) {
-            throw Error(`todoData не массив, а ${typeof todoData}`)
-          }
-
-//  -------------------
-
+        const visibleItems = this.filter(
+            this.search(todoData, term), filter)
         const doneCount = todoData.filter((el) => el.done).length
         const todoCount = todoData.length - doneCount
     
         return (
-          <div className = 'todo-app'>
-            <AppHeader toDo = {todoCount} done = {doneCount} />
-            <div className = 'top-panel'>
-              <SearchPanel />
-              <ItemStatusFilter />
+            <div className = 'todo-app'>
+                <AppHeader toDo = {todoCount} done = {doneCount} />
+                <div className = 'top-panel'>
+                    <SearchPanel 
+                        onSearchChange = {this.onSearchChange} />
+                    <ItemStatusFilter 
+                        filter = {filter}
+                        onFilterChange = {this.onFilterChange} />
+                </div>
+
+                <TodoList
+                    todos = {visibleItems}
+                    onDeleted = { this.deleteItem }
+                    onToggleImportant = { this.onToggleImportant }
+                    onToggleDone = { this.onToggleDone }
+                />
+
+                <ItemAddForm onItemAdded = { this.addItem } />
             </div>
-    
-            <TodoList
-              todos = {todoData}
-              onDeleted = { this.deleteItem }
-              onToggleImportant = { this.onToggleImportant }
-              onToggleDone = { this.onToggleDone }
-            />
-    
-            <ItemAddForm onItemAdded = { this.addItem } />
-          </div>
         );
       }
 }
--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,15 +4,44 @@ import './item-add-form.css'
 
 export default class ItemAddForm extends Component {
 
+    state = {
+        label: ''
+    }
+
+    onLabelChange = (e) => {
+        this.setState ({
+            label: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+
+        if (this.state.label.length > 0) {
+
+            this.props.onItemAdded(this.state.label)
+            this.setState({
+                label: ''
+            })
+        }
+    }
+
     render() {
         return (
-            <div className = 'item-add-form'>
+            <form className = 'item-add-form'
+                  onSubmit = {this.onSubmit}>
+                <input 
+                    type = 'text' 
+                    className = 'form-control'
+                    onChange = {this.onLabelChange}
+                    placeholder = 'What needs to be done' 
+                    value = {this.state.label} />
+
                 <button 
-                    className = 'btn btn-outline-secondary'
-                    onClick = {() => this.props.addItem(' Hello World ') } >
+                    className = 'btn btn-outline-secondary' >
                     Add Item 
                 </button>
-            </div>
+            </form>
         )
     }
 }
